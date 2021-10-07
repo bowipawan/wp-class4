d8fbbe23e41b09a0ff29080ef4b847d4279fac95
bowipawan <bowipawan@hotmail.com>
2021-10-07 09:22:17 +0700

    edit homework class6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,19 +53,16 @@ class UsersController < ApplicationController
   # DELETE /users/1 or /users/1.json
   def destroy
     @user.destroy
-    # respond_to do |format|
-      # format.html { redirect_to users_url, notice: "User was successfully destroyed." }
-      # format.json { head :no_content }
-    # end
+    session[:user_id] = nil
   end
 
   # create_fast
   def create_fast
     @user = User.create(name:params[:name], email:params[:email])
-    session[:user_id] = @user.id
 
     respond_to do |format|
       if @user.save
+        session[:user_id] = @user.id
         format.html { redirect_to @user, notice: "User was successfully created." }
         format.json { render :show, status: :created, location: @user }
       else
